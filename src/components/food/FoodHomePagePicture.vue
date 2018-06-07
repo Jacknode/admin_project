6f683740c4ff739e2885760bd97f8207735056a6
unknown <you@example.com>
2018-06-07 18:06:28 +0800

    gjh

--- src/components/food/FoodHomePagePicture.vue
+++ src/components/food/FoodHomePagePicture.vue
@@ -207,16 +207,40 @@
             })
         })
       },
+      uploadToOSS(file) {
+        return new Promise((relove,reject)=>{
+          var fd = new FormData();
+          fd.append("fileToUpload", file);
+          var xhr = new XMLHttpRequest();
+          xhr.open("POST", "http://webservice.1000da.com.cn/OSSFile/PostToOSS");
+          xhr.send(fd);
+          xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+              if (xhr.responseText) {
+                var data = xhr.responseText
+                relove(JSON.parse(data))
+              }
+            }else{
+              console.log(xhr.responseText)
+//               if (xhr.responseText) {
+//                 var data = xhr.responseText;
+//                 reject(JSON.parse(data).resultcontent)
+//               }
+            }
+          }
+        })
+      },
       uploaNode() {
         setTimeout(() => {
           this.addOptions.fd_st_ImageURL = '';
           if (this.$refs.upload) {
             this.$refs.upload.addEventListener('change', data => {
               for (var i = 0; i < this.$refs.upload.files.length; i++) {
-                this.uploadImg(this.$refs.upload.files[i]).then(data => {
-                  this.$store.dispatch('uploadAdminImgs', {
-                    imageData: data
-                  })
+                // this.uploadImg(this.$refs.upload.files[i]).then(data => {
+                //   this.$store.dispatch('uploadAdminImgs', {
+                //     imageData: data
+                //   })
+                this.uploadToOSS(this.$refs.upload.files[i])
                     .then(data => {
                       if (data) {
                         this.addOptions.fd_st_ImageURL = data.data;
@@ -227,7 +251,7 @@
                         });
                       }
                     })
-                })
+                // })
               }
             })
           }
